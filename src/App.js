3a3939a99cb18c6a7ec1b53a2e4ad52af2507991
Palina Skakun <palinaskakun@gmail.com>
2024-03-25 22:53:53 -0400

    text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Palina Skakun is learning React!😍
         </a>
       </header>
     </div>
